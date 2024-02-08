6f4845b655ef856b1628f2048c9cc122d523aef3
Scott Laboe <scottyl2.718@gmail.com>
2024-02-08 17:25:33 +0000

    test(renode): Add iworx boards to renode script
     - Add boards prefixed with "iworx" and postifixed with "nrf52840" to renode .repl generation script.

--- scripts/renode/genRenodeRepl.sh
+++ scripts/renode/genRenodeRepl.sh
@@ -3,24 +3,40 @@
 #First parameter is the name of the board
 BOARD_NAME="$1"
 BOARD_PREFIX=${BOARD_NAME%%_*}
+BOARD_POSTFIX=${BOARD_NAME##*_}
 
-if [ "$ZEPHYR_BASE" = "" ]
-then
+# Check if ZEPHYR_BASE is set
+if [ -z ${ZEPHYR_BASE+x} ]; then
     ZEPHYR_BASE="$(pwd)/zephyr"
     echo "WARNING: ZEPHYR_BASE is unset; using \"$ZEPHYR_BASE\""
 fi
 
-OUT_FILE="${ZEPHYR_BASE}/boards/arm/${BOARD_NAME}/support/${BOARD_NAME}.repl"
+echo "ZEPHYR_BASE: $ZEPHYR_BASE"
+
+# Check if BOARD_ROOT is set
+if [ -z ${BOARD_ROOT+x} ]; then
+    BOARD_ROOT="$ZEPHYR_BASE"
+    echo "WARNING: BOARD_ROOT is unset; using ZEPHYR_ROOT:\"$BOARD_ROOT\""
+fi
+
+echo "BOARD_ROOT: $BOARD_ROOT"
+
+OUT_FILE="${BOARD_ROOT}/boards/arm/${BOARD_NAME}/support/${BOARD_NAME}.repl"
 
 #Include board specific files
-if [ "$BOARD_PREFIX" = "nucleo" ]
-then
+if [ "$BOARD_PREFIX" = "nucleo" ]; then
     BOARD_INC="\
         -I ${ZEPHYR_BASE}/include \
         -I ${ZEPHYR_BASE}/dts \
         -I ${ZEPHYR_BASE}/dts/arm \
         -I ${ZEPHYR_BASE}/dts/common \
         -I ${ZEPHYR_BASE}/../modules/hal/stm32/dts"
+elif test "$BOARD_PREFIX" = "iworx" && test "$BOARD_POSTFIX" = "nrf52840"; then
+    BOARD_INC="\
+        -I ${ZEPHYR_BASE}/include \
+        -I ${ZEPHYR_BASE}/dts \
+        -I ${ZEPHYR_BASE}/dts/arm \
+        -I ${ZEPHYR_BASE}/dts/common"
 fi
 
 #Generate files
@@ -32,7 +48,7 @@ ${ZEPHYR_SDK_INSTALL_DIR}/arm-zephyr-eabi/bin/arm-zephyr-eabi-gcc \
     -H -E -P \
     -x assembler-with-cpp \
     ${BOARD_INC} \
-    ${ZEPHYR_BASE}/boards/arm/${BOARD_NAME}/${BOARD_NAME}.dts \
+    ${BOARD_ROOT}/boards/arm/${BOARD_NAME}/${BOARD_NAME}.dts \
     1>${FLAT_DTS_FILE} 2>${INCLUDES_FILE}
 
 echo "Generating Renode's .repl file"
